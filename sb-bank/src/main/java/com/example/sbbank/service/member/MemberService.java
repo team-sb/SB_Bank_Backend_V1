b9635f2f939db91b38bcd39e4b92f36e9bc51bf6
김범진 <sdpthf@naver.com>
2021-08-19 23:22:25 +0900

    [ADD] MemberService

---
+++ sb-bank/src/main/java/com/example/sbbank/service/member/MemberService.java
@@ -0,0 +1,26 @@
+package com.example.sbbank.service.member;
+
+import com.example.sbbank.entity.member.MemberRepository;
+import com.example.sbbank.payload.MemberRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public String join(MemberRequest request) {
+        if(memberRepository.existsByNameOrUsername(request.getName(), request.getUsername())) {
+            throw new RuntimeException();
+        }
+
+        request.setPassword(bCryptPasswordEncoder.encode(request.getPassword()));
+        memberRepository.save(request.toEntity());
+
+        return "success join";
+    }
+
+}
