0139fc0a1b2307a7aba384fd599a44f85e3790d9
김범진 <sdpthf@naver.com>
2021-08-19 23:18:54 +0900

    SbBankApplication

---
+++ sb-bank/src/main/java/com/example/sbbank/SbBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbBankApplication.class, args);
+    }
+
+}
