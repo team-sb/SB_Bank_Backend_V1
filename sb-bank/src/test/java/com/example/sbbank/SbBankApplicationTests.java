2274330aaedea49191655ee84014817c0625dad7
김범진 <sdpthf@naver.com>
2021-08-19 23:18:39 +0900

    SbBankApplicationTests

---
+++ sb-bank/src/test/java/com/example/sbbank/SbBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
