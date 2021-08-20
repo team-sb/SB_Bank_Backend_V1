4b069116671e952157ef6fb5cada029fd995ebb2
김범진 <sdpthf@naver.com>
2021-08-20 17:13:31 +0900

    [ADD] TokenResponse

---
+++ sb-bank/src/main/java/com/example/sbbank/payload/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.example.sbbank.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String token;
+}
