11f14d9c93534f401110dca2bab2c21bc32f6c4a
김범진 <sdpthf@naver.com>
2021-08-20 17:13:08 +0900

    [ADD] MemberLoginRequestDto

---
+++ sb-bank/src/main/java/com/example/sbbank/payload/MemberLoginRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.sbbank.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginRequestDto {
+    private String username;
+    private String password;
+}
