acc9187dbf39396394ef140ec9275ec1c29083db
김범진 <sdpthf@naver.com>
2021-08-21 01:17:24 +0900

    [UPDATE] AccountController

--- sb-bank/src/main/java/com/example/sbbank/controller/account/AccountController.java
+++ sb-bank/src/main/java/com/example/sbbank/controller/account/AccountController.java
@@ -1,19 +1,32 @@
 package com.example.sbbank.controller.account;
 
-import com.example.sbbank.payload.AccountRequest;
+import com.example.sbbank.payload.request.AccountRegisterRequestDto;
+import com.example.sbbank.payload.request.TransferRequestDto;
+import com.example.sbbank.payload.response.AccountRegisterResponseDto;
+import com.example.sbbank.security.auth.CustomUserDetails;
 import com.example.sbbank.service.account.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/account")
 public class AccountController {
     private final AccountService accountService;
 
     @PostMapping("/register")
-    public String register(@RequestBody AccountRequest request) {
-        return accountService.register(request);
+    public AccountRegisterResponseDto register(@RequestBody AccountRegisterRequestDto requestDto,
+                                               @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return accountService.register(requestDto, userDetails.getMember());
+    }
+
+    @PostMapping("/record")
+    public String record(@RequestBody TransferRequestDto request,
+                           @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return accountService.record(request, userDetails.getMember());
     }
 }
