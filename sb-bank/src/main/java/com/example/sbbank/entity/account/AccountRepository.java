fc3b2edb316394831f338e5d4fb1b547d9695ba5
김범진 <sdpthf@naver.com>
2021-08-21 01:17:54 +0900

    [ADD] AccountRepository

---
+++ sb-bank/src/main/java/com/example/sbbank/entity/account/AccountRepository.java
@@ -0,0 +1,6 @@
+package com.example.sbbank.entity.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+}
