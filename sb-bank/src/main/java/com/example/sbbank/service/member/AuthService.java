e2ec959e4d63bc4907f92c25bb193da472a93a71
김범진 <sdpthf@naver.com>
2021-08-20 17:15:55 +0900

    [UPDATE] AuthService

--- sb-bank/src/main/java/com/example/sbbank/service/member/AuthService.java
+++ sb-bank/src/main/java/com/example/sbbank/service/member/AuthService.java
@@ -1,26 +1,42 @@
 package com.example.sbbank.service.member;
 
+import com.example.sbbank.entity.member.Member;
 import com.example.sbbank.entity.member.MemberRepository;
-import com.example.sbbank.payload.MemberRequest;
+import com.example.sbbank.security.jwt.JwtTokenProvider;
+import com.example.sbbank.payload.MemberLoginRequestDto;
+import com.example.sbbank.payload.MemberJoinRequestDto;
+import com.example.sbbank.payload.TokenResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final MemberRepository memberRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider tokenProvider;
 
-    public String join(MemberRequest request) {
+    public String join(MemberJoinRequestDto request) {
         if(memberRepository.existsByNameOrUsername(request.getName(), request.getUsername())) {
             throw new RuntimeException();
         }
 
-        request.setPassword(bCryptPasswordEncoder.encode(request.getPassword()));
+        request.setPassword(passwordEncoder.encode(request.getPassword()));
         memberRepository.save(request.toEntity());
 
         return "success join";
+    }
+
+    public TokenResponse login(MemberLoginRequestDto request) {
+        Member member = memberRepository.findByUsername(request.getUsername())
+                .orElseThrow();
+
+        if(!passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException();
+        }
+
+        return tokenProvider.createToken(member.getUsername(), member.getAuthority());
     }
 
 }
