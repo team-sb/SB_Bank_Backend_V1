9f8033aa91442fc01dff545b351a662aba6445c3
김범진 <sdpthf@naver.com>
2021-08-20 17:14:06 +0900

    [ADD] CustomUserDetailsService

---
+++ sb-bank/src/main/java/com/example/sbbank/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.sbbank.security.auth;
+
+import com.example.sbbank.entity.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다."));
+    }
+}
