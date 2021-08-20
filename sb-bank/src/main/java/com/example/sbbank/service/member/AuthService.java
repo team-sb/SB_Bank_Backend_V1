667b77eeb8dea09b0e852dbcb11b0b05c2dc27cd
김범진 <sdpthf@naver.com>
2021-08-20 13:00:00 +0900

    Change name

--- sb-bank/src/main/java/com/example/sbbank/service/member/AuthService.java
+++ sb-bank/src/main/java/com/example/sbbank/service/member/AuthService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class AuthService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
