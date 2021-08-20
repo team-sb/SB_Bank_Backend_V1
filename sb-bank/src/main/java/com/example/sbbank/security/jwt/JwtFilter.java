757da1e456712f9f60336b8af87d925f8dd8aa7d
김범진 <sdpthf@naver.com>
2021-08-20 17:15:22 +0900

    [ADD] JwtFilter

---
+++ sb-bank/src/main/java/com/example/sbbank/security/jwt/JwtFilter.java
@@ -0,0 +1,32 @@
+package com.example.sbbank.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request
+            , HttpServletResponse response
+            , FilterChain filterChain) throws ServletException, IOException {
+
+        String token = tokenProvider.resolveToken(request);
+
+        if(token != null && tokenProvider.validateToken(token)) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
