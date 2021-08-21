b2031acb32698d61b9e7edbdf70fe9bfb18c13aa
김범진 <sdpthf@naver.com>
2021-08-21 23:40:12 +0900

    [ADD] ErrorCode

---
+++ sb-bank/src/main/java/com/example/sbbank/error/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.example.sbbank.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INVALID_TOKEN(401, "Invalid Token"),
+    INVALID_PASSWORD(401, "Invalid Password"),
+
+    USER_ALREADY_EXISTS(409, "User Already Exists"),
+    USER_NOT_FOUND(404, "User Not Found");
+
+    private final int status;
+    private final String message;
+}
