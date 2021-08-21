7e1289620234c285469a5bf376955cafbc9cb956
김범진 <sdpthf@naver.com>
2021-08-21 18:39:57 +0900

    [ADD] AccountChargeRequestDto

---
+++ sb-bank/src/main/java/com/example/sbbank/payload/request/AccountChargeRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.sbbank.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountChargeRequestDto {
+    private Integer money;
+}
