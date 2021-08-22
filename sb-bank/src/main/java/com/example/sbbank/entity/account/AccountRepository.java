26b937d75c6e2cee2d109230d0f6fc7c2d18f6b7
김범진 <sdpthf@naver.com>
2021-08-22 14:38:12 +0900

    [UPDATE] 유저 아이디로 찾기

--- sb-bank/src/main/java/com/example/sbbank/entity/account/AccountRepository.java
+++ sb-bank/src/main/java/com/example/sbbank/entity/account/AccountRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByMemberId(Integer id);
     Optional<Account> findByAccountNumber(Integer target);
 }
