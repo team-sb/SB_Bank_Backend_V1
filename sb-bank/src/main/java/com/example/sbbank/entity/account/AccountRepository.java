21636b718836d59630c0f5b277c810442aa39af6
김범진 <sdpthf@naver.com>
2021-08-22 14:01:12 +0900

    [UPDATE] 계좌 번호 찾기

--- sb-bank/src/main/java/com/example/sbbank/entity/account/AccountRepository.java
+++ sb-bank/src/main/java/com/example/sbbank/entity/account/AccountRepository.java
@@ -2,5 +2,8 @@ package com.example.sbbank.entity.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByAccountNumber(Integer target);
 }
