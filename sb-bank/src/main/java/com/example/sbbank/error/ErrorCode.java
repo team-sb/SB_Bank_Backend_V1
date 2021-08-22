7072ceaa7c8983d9d2b9eb78f5090c9516f64b65
김범진 <sdpthf@naver.com>
2021-08-23 08:17:16 +0900

    [UPDATE] AccountNotFoundException

--- sb-bank/src/main/java/com/example/sbbank/error/ErrorCode.java
+++ sb-bank/src/main/java/com/example/sbbank/error/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     INVALID_PASSWORD(401, "Invalid Password"),
 
     USER_ALREADY_EXISTS(409, "User Already Exists"),
-    USER_NOT_FOUND(404, "User Not Found");
+    USER_NOT_FOUND(404, "User Not Found"),
+
+    ACCOUNT_NOT_FOUND(404, "Account Not Found");
 
     private final int status;
     private final String message;
