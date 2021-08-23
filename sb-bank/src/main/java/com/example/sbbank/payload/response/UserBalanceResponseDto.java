f9c8ee8244370c8a67b3975d4085561b2992f0af
김범진 <sdpthf@naver.com>
2021-08-23 23:14:11 +0900

    [UPDATE] UserBalanceResponseDto

--- sb-bank/src/main/java/com/example/sbbank/payload/response/UserBalanceResponseDto.java
+++ sb-bank/src/main/java/com/example/sbbank/payload/response/UserBalanceResponseDto.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class BalanceResponseDto {
+public class UserBalanceResponseDto {
     private Integer balance;
 }
