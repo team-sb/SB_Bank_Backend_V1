32a47b9911fbe3d43af2878b2459ba65b1d31d7d
김범진 <sdpthf@naver.com>
2021-08-23 20:14:31 +0900

    [UPDATE] EncodingSecPassword

--- sb-bank/src/main/java/com/example/sbbank/service/auth/AuthService.java
+++ sb-bank/src/main/java/com/example/sbbank/service/auth/AuthService.java
@@ -31,7 +31,7 @@ public class AuthService {
                         .name(request.getName())
                         .username(request.getUsername())
                         .password(passwordEncoder.encode(request.getPassword()))
-                        .secPassword(request.getSecPassword())
+                        .secPassword(passwordEncoder.encode(request.getSecPassword()))
                         .authority(Authority.ROLE_USER)
                         .build());
 
