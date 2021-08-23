076ffd4d04b18dbf84914a9c1dd7700bea64173e
김범진 <sdpthf@naver.com>
2021-08-23 23:13:50 +0900

    [UPDATE] FindByMemberId

--- sb-bank/src/main/java/com/example/sbbank/entity/account/RecordRepository.java
+++ sb-bank/src/main/java/com/example/sbbank/entity/account/RecordRepository.java
@@ -2,5 +2,8 @@ package com.example.sbbank.entity.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    List<Record> findByMemberId(Integer id);
 }
