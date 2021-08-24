e8d72cb10c092242398b6564036d4ccdbaee8f82
김범진 <sdpthf@naver.com>
2021-08-24 17:13:16 +0900

    [UPDATE] AccessTokenResponse

--- sb-bank/src/main/java/com/example/sbbank/payload/response/AccessTokenResponseDto.java
+++ sb-bank/src/main/java/com/example/sbbank/payload/response/AccessTokenResponseDto.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TokenResponseDto {
+public class AccessTokenResponseDto {
     private String accessToken;
 }
