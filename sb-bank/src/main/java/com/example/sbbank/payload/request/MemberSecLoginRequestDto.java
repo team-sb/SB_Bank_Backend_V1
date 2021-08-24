37d6e11e11293ed94fa80d30718d3cefce5abdea
김범진 <sdpthf@naver.com>
2021-08-24 17:12:52 +0900

    [UPDATE] SecLoginRequest

---
+++ sb-bank/src/main/java/com/example/sbbank/payload/request/MemberSecLoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.sbbank.payload.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberSecLoginRequestDto {
+
+    @JsonProperty("sec_password")
+    private String secPassword;
+}
