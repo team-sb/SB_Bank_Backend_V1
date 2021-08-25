120132dc9669d425d0d9d891cfaf7bfa78e82085
김범진 <sdpthf@naver.com>
2021-08-25 19:04:13 +0900

    [UPDATE] LoanRepository

--- sb-bank/src/main/java/com/example/sbbank/entity/account/record/loan/LoanRepository.java
+++ sb-bank/src/main/java/com/example/sbbank/entity/account/record/loan/LoanRepository.java
@@ -2,5 +2,8 @@ package com.example.sbbank.entity.account.record.loan;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LoanRepository extends JpaRepository<LoanRecord, Integer> {
+    Optional<LoanRecord> findByMemberId(Integer id);
 }
