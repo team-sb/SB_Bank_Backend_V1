3b0516716f52d73cb0e106ee1e510cee4379195f
김범진 <sdpthf@naver.com>
2021-08-25 19:04:47 +0900

    [UPDATE] BorrowResponse

--- sb-bank/src/main/java/com/example/sbbank/payload/response/AccountBorrowResponseDto.java
+++ sb-bank/src/main/java/com/example/sbbank/payload/response/AccountBorrowResponseDto.java
@@ -1,21 +1,15 @@
 package com.example.sbbank.payload.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.util.Date;
-import java.util.List;
 
 @Getter
-@NoArgsConstructor
+@Builder
 @AllArgsConstructor
 public class AccountBorrowResponseDto {
-    private List<Borrow> borrows;
-
-    public static class Borrow {
         private Integer money;
         private Double interest;
-        private Date borrowExpirationDate;
-    }
+        private Date loanExpirationDate;
 }
