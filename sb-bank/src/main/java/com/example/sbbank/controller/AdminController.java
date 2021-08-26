311e107062e2099034eb1e69e068eacecc42852d
김범진 <sdpthf@naver.com>
2021-08-26 19:17:04 +0900

    [UPDATE] ShowUserAccountInfoController

--- sb-bank/src/main/java/com/example/sbbank/controller/AdminController.java
+++ sb-bank/src/main/java/com/example/sbbank/controller/AdminController.java
@@ -1,9 +1,11 @@
 package com.example.sbbank.controller;
 
+import com.example.sbbank.payload.response.ShowUserAccountInfoResponseDto;
 import com.example.sbbank.payload.response.ShowUserListResponseDto;
 import com.example.sbbank.service.admin.AdminServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ public class AdminController {
     @GetMapping("/users")
     public ShowUserListResponseDto showUsers() {
         return adminService.showUsers();
+    }
+
+    @GetMapping("/users/{user-id}")
+    public ShowUserAccountInfoResponseDto showUserAccountInfo(@PathVariable(name = "user-id") Integer id) {
+        return adminService.showUserAccountInfo(id);
     }
 
 }
