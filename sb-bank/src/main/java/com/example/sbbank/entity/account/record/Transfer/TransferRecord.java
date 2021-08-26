efae09f55179f9e19ca30310888ad32ea608a777
김범진 <sdpthf@naver.com>
2021-08-26 10:02:39 +0900

    [UPDATE] Transfer

--- sb-bank/src/main/java/com/example/sbbank/entity/account/record/Transfer/TransferRecord.java
+++ sb-bank/src/main/java/com/example/sbbank/entity/account/record/Transfer/TransferRecord.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name = "transfer")
 public class TransferRecord {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
