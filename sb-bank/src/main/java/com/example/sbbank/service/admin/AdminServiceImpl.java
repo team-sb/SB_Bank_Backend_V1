aeacce3c8dca7f609b8681747207fb73d71a32a3
김범진 <sdpthf@naver.com>
2021-08-26 19:12:33 +0900

    [UPDATE] ShowUsersService

--- sb-bank/src/main/java/com/example/sbbank/service/admin/AdminServiceImpl.java
+++ sb-bank/src/main/java/com/example/sbbank/service/admin/AdminServiceImpl.java
@@ -1,4 +1,32 @@
 package com.example.sbbank.service.admin;
 
-public class AdminServiceImpl {
+import com.example.sbbank.entity.member.MemberRepository;
+import com.example.sbbank.payload.response.ShowUserListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AdminServiceImpl implements AdminService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public ShowUserListResponseDto showUsers() {
+        List<ShowUserListResponseDto.users> userList = memberRepository.findAll()
+                .stream()
+                .map(member -> new ShowUserListResponseDto.users(
+                        member.getId(),
+                        member.getName(),
+                        member.getUsername(),
+                        member.getAuthority(),
+                        member.getAccount().getAccountNumber()
+                ))
+                .collect(Collectors.toList());
+
+        return new ShowUserListResponseDto(userList);
+    }
+
 }
