daf55d329585def5b6c2e0247708fffd7d764987
김범진 <sdpthf@naver.com>
2021-08-26 20:17:36 +0900

    [UPDATE] UpdateUserBalance

--- sb-bank/src/main/java/com/example/sbbank/service/admin/AdminService.java
+++ sb-bank/src/main/java/com/example/sbbank/service/admin/AdminService.java
@@ -1,9 +1,11 @@
 package com.example.sbbank.service.admin;
 
+import com.example.sbbank.payload.request.UpdateUserBalanceRequestDto;
 import com.example.sbbank.payload.response.ShowUserAccountInfoResponseDto;
 import com.example.sbbank.payload.response.ShowUserListResponseDto;
 
 public interface AdminService {
     ShowUserListResponseDto showUsers();
     ShowUserAccountInfoResponseDto showUserAccountInfo(Integer id);
+    String updateUserBalance(UpdateUserBalanceRequestDto request, Integer id);
 }
--- sb-bank/src/main/java/com/example/sbbank/service/admin/AdminServiceImpl.java
+++ sb-bank/src/main/java/com/example/sbbank/service/admin/AdminServiceImpl.java
@@ -3,14 +3,17 @@ package com.example.sbbank.service.admin;
 import com.example.sbbank.entity.account.AccountRepository;
 import com.example.sbbank.entity.member.MemberRepository;
 import com.example.sbbank.exception.UserNotFoundException;
+import com.example.sbbank.payload.request.UpdateUserBalanceRequestDto;
 import com.example.sbbank.payload.response.ShowUserAccountInfoResponseDto;
 import com.example.sbbank.payload.response.ShowUserListResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService {
 
@@ -44,6 +47,20 @@ public class AdminServiceImpl implements AdminService {
                         account.getBalance()
                 ))
                 .orElseThrow(UserNotFoundException::new);
+    }
+
+    @Override
+    public String updateUserBalance(UpdateUserBalanceRequestDto request, Integer id) {
+
+        accountRepository.findByMemberId(id)
+                .map(account -> {
+                    account.setBalance(request.getMoney());
+                    accountRepository.save(account);
+                    return account;
+                })
+                .orElseThrow();
+
+        return "success update";
     }
 
 }
