24d684f32eb7112fc0d1236a0c7aff2c30707acf
김범진 <sdpthf@naver.com>
2021-08-30 23:08:40 +0900

    [UPDATE] AdminController

--- sb-bank/src/main/java/com/example/sbbank/controller/AdminController.java
+++ sb-bank/src/main/java/com/example/sbbank/controller/AdminController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     private final AdminServiceImpl adminService;
 
-    @GetMapping
+    @GetMapping("")
     public ShowUserListResponseDto showUsers() {
         return adminService.showUsers();
     }
